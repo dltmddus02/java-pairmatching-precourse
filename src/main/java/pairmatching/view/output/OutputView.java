435bc71f4c04f41c18689741535cd810824df38b
dltmddus <tmddus4671@gmail.com>
2024-12-12 19:36:30 +0900

    feat(OutputView) : 과정, 레벨, 미션 출력 기능 구현

--- src/main/java/pairmatching/view/output/OutputView.java
+++ src/main/java/pairmatching/view/output/OutputView.java
@@ -1,9 +1,14 @@
 package pairmatching.view.output;
 
+import static pairmatching.view.output.OutputMessage.INPUT_COURSE_LEVEL_MISSION;
 import static pairmatching.view.output.OutputMessage.INPUT_FEATURE;
 
 public class OutputView {
     public static void printFeature() {
         System.out.println(INPUT_FEATURE.getMessage());
+    }
+
+    public static void printCourseLevelMission() {
+        System.out.println(INPUT_COURSE_LEVEL_MISSION.getMessage());
     }
 }
