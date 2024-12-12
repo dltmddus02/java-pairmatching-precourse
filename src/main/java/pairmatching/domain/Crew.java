3df568cbf1c9e67a9a228624d9bdd562c2f5e5eb
dltmddus <tmddus4671@gmail.com>
2024-12-12 21:39:33 +0900

    fix(Crew, CrewRepository) : Crew 정보를 CrewRepository에 저장하도록 구현

--- src/main/java/pairmatching/domain/Crew.java
+++ src/main/java/pairmatching/domain/Crew.java
@@ -1,14 +1,36 @@
 package pairmatching.domain;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Crew {
     private final String name;
-    private List<Crew> matchedCrew;
+    // 주니, 레벨1
+    private Map<String, String> matchedCrew;
 
     public Crew(String name) {
         this.name = name;
-        this.matchedCrew = new ArrayList<>();
+        this.matchedCrew = new HashMap<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addMatchedCrew(String name, String level) {
+        this.matchedCrew.put(name, level);
+    }
+
+    public boolean isExistName(String name) {
+        return this.name.equals(name);
+    }
+
+    public boolean isAlreadyMatchedWith(Crew c, String level) {
+        for (String name : matchedCrew.keySet()) {
+            if (name.equals(c.name) && matchedCrew.get(name).equals(level)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/pairmatching/domain/CrewRepository.java
+++ src/main/java/pairmatching/domain/CrewRepository.java
@@ -1,22 +1,54 @@
 package pairmatching.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class CrewRepository {
-    private List<Crew> backEndCrew;
-    private List<Crew> frontEndCrew;
+    private static final List<Crew> crews = new ArrayList<>();
 
-    public CrewRepository() {
-        this.backEndCrew = new ArrayList<>();
-        this.frontEndCrew = new ArrayList<>();
+    private static List<String> backEndCrew = new ArrayList<>();
+    private static List<String> frontEndCrew = new ArrayList<>();
+
+    public static List<Crew> crews() {
+        return Collections.unmodifiableList(crews);
     }
 
-    public void addBackEndCrew(Crew c) {
-        this.backEndCrew.add(c);
+
+    public static List<String> getBackEndCrew() {
+        return Randoms.shuffle(backEndCrew);
     }
 
-    public void addFrontEndCrew(Crew c) {
-        this.frontEndCrew.add(c);
+    public static List<String> getFrontEndCrew() {
+        return Randoms.shuffle(frontEndCrew);
     }
+
+    public static void addCrew(Crew crew) {
+        crews.add(crew);
+    }
+
+    public static boolean deleteCrew(String name) {
+        return crews.removeIf(crew -> Objects.equals(crew.getName(), name));
+    }
+
+    public static Crew findCrewByName(String name) {
+        for (Crew crew : crews()) {
+            if (crew.isExistName(name)) {
+//                matchedCrew에 매칭됐던 애인지 보기
+                return crew;
+            }
+        }
+        return null;
+    }
+
+    public static void addBackEndCrew(String c) {
+        backEndCrew.add(c);
+    }
+
+    public static void addFrontEndCrew(String c) {
+        frontEndCrew.add(c);
+    }
+
 }
