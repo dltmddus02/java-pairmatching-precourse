6527612b82666b8672390088a8c771f224fefde7
dltmddus <tmddus4671@gmail.com>
2024-12-12 22:24:23 +0900

    feat(LevelMissions) : 매칭 결과 List<String>으로 저장하는 LevelMissions 도메인 생성

---
+++ src/main/java/pairmatching/domain/LevelMissions.java
@@ -0,0 +1,38 @@
+package pairmatching.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LevelMissions {
+    private final String course;
+    private final String level;
+    private final String mission;
+    private List<String> matchingResult;
+
+    public LevelMissions(String course, String level, String mission) {
+        this.course = course;
+        this.level = level;
+        this.mission = mission;
+        this.matchingResult = new ArrayList<>();
+    }
+
+    public boolean getCourse(String course) {
+        return this.course.equals(course);
+    }
+
+    public boolean getLevel(String level) {
+        return this.level.equals(level);
+    }
+
+    public boolean getMission(String mission) {
+        return this.mission.equals(mission);
+    }
+
+    public void setMatchingResult(List<String> matchingResult) {
+        this.matchingResult = matchingResult;
+    }
+
+    public boolean matchingResultAlreadyExist() {
+        return !this.matchingResult.isEmpty();
+    }
+}
