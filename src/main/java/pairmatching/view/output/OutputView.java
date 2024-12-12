e4b28b8c39fc6d88436ca05af322852c8b3e99e2
dltmddus <tmddus4671@gmail.com>
2024-12-12 19:26:46 +0900

    feat(OutputView) : 실행할 기능 출력 기능 구현

---
+++ src/main/java/pairmatching/view/output/OutputView.java
@@ -0,0 +1,9 @@
+package pairmatching.view.output;
+
+import static pairmatching.view.output.OutputMessage.INPUT_FEATURE;
+
+public class OutputView {
+    public static void printFeature() {
+        System.out.println(INPUT_FEATURE.getMessage());
+    }
+}
