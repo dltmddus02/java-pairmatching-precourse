5cfb41aff32c75c983929001c9232f1780579024
dltmddus <tmddus4671@gmail.com>
2024-12-12 21:02:15 +0900

    feat(OutputView) : 매칭 결과 출력 기능 구현

--- src/main/java/pairmatching/view/output/OutputView.java
+++ src/main/java/pairmatching/view/output/OutputView.java
@@ -1,14 +1,38 @@
 package pairmatching.view.output;
 
+import static pairmatching.view.output.OutputMessage.FEATURE_TYPE;
 import static pairmatching.view.output.OutputMessage.INPUT_COURSE_LEVEL_MISSION;
 import static pairmatching.view.output.OutputMessage.INPUT_FEATURE;
+import static pairmatching.view.output.OutputMessage.MATCHING_RESULT;
+import static pairmatching.view.output.OutputMessage.PAIR;
+import static pairmatching.view.output.OutputMessage.TRIPLE;
+
+import java.util.List;
 
 public class OutputView {
     public static void printFeature() {
         System.out.println(INPUT_FEATURE.getMessage());
+        System.out.println(FEATURE_TYPE.getMessage());
     }
 
     public static void printCourseLevelMission() {
         System.out.println(INPUT_COURSE_LEVEL_MISSION.getMessage());
+    }
+
+    public static void printMatchingResult(List<String> shuffledCrew) {
+        System.out.println(MATCHING_RESULT.getMessage());
+        if (shuffledCrew.size() % 2 == 0) {
+            for (int idx = 0; idx < shuffledCrew.size() / 2; idx++) {
+                System.out.printf(PAIR.getMessage(), shuffledCrew.get(idx), shuffledCrew.get(idx + 1));
+            }
+            return;
+        }
+        for (int idx = 0; idx < shuffledCrew.size() / 2 - 1; idx++) {
+            System.out.printf(PAIR.getMessage(), shuffledCrew.get(idx), shuffledCrew.get(idx + 1));
+        }
+        System.out.printf(TRIPLE.getMessage(), shuffledCrew.get(shuffledCrew.size() - 3),
+                shuffledCrew.get(shuffledCrew.size() - 2),
+                shuffledCrew.get(shuffledCrew.size() - 1));
+
     }
 }
